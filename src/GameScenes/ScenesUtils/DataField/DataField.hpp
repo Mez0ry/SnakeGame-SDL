5b6df45ba3afe34cafb5fd95db94b14bfc26004c
Mez0ry <612315123@mail.ru>
2022-12-29 17:28:27 +0300

    Refactoring of 'DataField' module

--- src/GameScenes/ScenesUtils/DataField/DataField.hpp
+++ src/GameScenes/ScenesUtils/DataField/DataField.hpp
@@ -15,29 +15,19 @@ typedef struct DataModel
 
 class DataField
 {
-    struct Range
-    {
-        int top, bottom;
-    } m_range;
-
 public:
     DataField();
     ~DataField();
-    void OnCreate(const std::string &texture_path, ImageType = ImageType::PNG);
 
     void Update();
     void Render();
 
     void LoadTexture(const std::string &texture_path, ImageType type = ImageType::PNG);
 
-    void set_Activity(bool activity_state);
-    bool isActive() const { return m_isActive; }
     DataModel &get_DataModel() { return m_DataModel; }
     TextureManager &get_Texture() { return m_DataFieldTexture; }
-    Range &get_Range() { return m_range; }
 
 private:
-    bool m_isActive = true;
     TextureManager m_DataFieldTexture;
     DataModel m_DataModel;
 };
