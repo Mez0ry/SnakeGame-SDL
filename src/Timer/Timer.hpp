4c6c2a5048624096818239309cf4e9f97b0850b1
Mez0ry <612315123@mail.ru>
2023-01-13 07:45:16 +0300

    Refactoring of 'Timer' module

--- src/Timer/Timer.hpp
+++ src/Timer/Timer.hpp
@@ -3,31 +3,19 @@
 #include <chrono>
 #include <iostream>
 
-#define COLOR_CYAN "\e[0;36m"
-
-enum TimeUnit{
- SECONDS,MILLISECONDS
-};
 
 class Timer {
-  private:
-  bool m_RAIItimer = false;
  public:
-  Timer() : m_RAIItimer(false) { };
-  Timer(const char* log_name);
-
+  Timer() { m_start = std::chrono::high_resolution_clock::now(); };
+  ~Timer();
   void Start();
   void Stop();
   int64_t GetDurationInMs();
-  int64_t GetDurationInSeconds();
-  
-  void SetLogName(const char* log_name) { m_LogName = log_name; }
-  ~Timer();
+  double GetDurationInSeconds();
 
  private:
   std::chrono::_V2::system_clock::time_point m_start, m_end;
   std::chrono::duration<double> m_duration;
-  const char* m_LogName;
 };
 
 #endif  //! SnakeGame_TIMER_HPP
